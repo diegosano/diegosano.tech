87707efc634b23944489eb44a07dbf62be33648d
Diego Sano <diego.sano@nave.rs>
2022-08-27 15:41:05 -0300

    feat: add favicon and meta description tag

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import { AnimatePresence } from 'framer-motion'
-import Head from 'next/head'
 
 import { Main } from '../layouts/Main'
 
@@ -11,13 +10,6 @@ export default function Website({ Component, pageProps, router }) {
     <ChakraProvider theme={theme}>
       <Main router={router}>
         <AnimatePresence mode="wait" initial={true}>
-          <Head>
-            <meta
-              name="viewport"
-              content="width=device-width, initial-scale=1"
-            />
-            <meta name="description" content="My personal website" />
-          </Head>
           <Component {...pageProps} key={router.route} />
         </AnimatePresence>
       </Main>
