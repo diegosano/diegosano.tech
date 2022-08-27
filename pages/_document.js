82cd78453736ab8b4296141cb0482088bb93d17c
Diego Sano <diego.sano@nave.rs>
2022-08-27 16:12:42 -0300

    chore: linting

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,11 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
           rel="stylesheet"
